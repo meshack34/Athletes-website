84b4a60b788ecacdedee02542341f33affbe540f
meshack34 <meshackkimutai34@gmail.com>
2024-07-26 19:41:25 +0300

    resonsiveness

--- athletes.js
+++ athletes.js
@@ -41,3 +41,12 @@ document.addEventListener('DOMContentLoaded', function() {
     //     changeSlide(1);
     // }, 6000);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.getElementById('nav-links');
+
+    menuToggle.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
+});
